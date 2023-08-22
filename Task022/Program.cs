f83ced9d2966d4c51894567c40fd27a518f872d7
Vladimir <bajiau@mail.ru>
2023-08-22 21:42:30 +0300

    Added Task022

---
+++ Task022/Program.cs
@@ -0,0 +1,17 @@
+﻿//Задача 22: Напишите программу, которая
+//принимает на вход число (N) и выдаёт таблицу
+//квадратов чисел от 1 до N.
+
+void WriteTableSquare(int number)
+{
+    int i = 1;
+    while (i <= number)
+    {
+        Console.WriteLine($"{i,3} -> {i * i,5}");
+        i++;
+    }
+}
+Console.WriteLine("Введите число");
+int n = Convert.ToInt32(Console.ReadLine());
+
+WriteTableSquare(n);
