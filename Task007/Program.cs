2107b6d16aa58ddf675985175a268de622d78ac6
MuromtsevVV <bajiau@mail.ru>
2023-08-23 14:34:19 +0300

    Added Task007

---
+++ Task007/Program.cs
@@ -0,0 +1,12 @@
+﻿// 07. Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+
+Console.Write("Ведите натуральное число N: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = -number;
+
+if (number < 0) number *= -1;
+while (count <= number)
+{
+    Console.Write($"{count} ");
+    count += 1;
+}
