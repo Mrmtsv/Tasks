31f71810495bacd0fa150a40e86cc2e9556f80b5
MuromtsevVV <bajiau@mail.ru>
2023-08-23 14:02:26 +0300

    United Task001

---
+++ Task001/Program.cs
@@ -0,0 +1,7 @@
+﻿// 01. Напишите программу, которая на вход принимает число и выдаёт его квадрат.
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int squared = number * number;
+
+Console.WriteLine($"{number}^2={squared}");
