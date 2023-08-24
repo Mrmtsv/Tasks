6860578caa7d75b53e52cdd1c18c1a2d9fa90cb2
MuromtsevVV <bajiau@mail.ru>
2023-08-24 14:37:21 +0300

    Corrected Task023_HW

--- Task023_HW/Program.cs
+++ Task023_HW/Program.cs
@@ -1,1 +1,16 @@
 ﻿// 23. Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 1)
+{
+    Console.WriteLine("Число должно быть больше нуля!");
+}
+else
+{
+    for (int i = 1; i <= number; i++)
+    {
+        Console.WriteLine($"Куб числа {i} равен {Math.Pow(i, 3)}");
+    }
+}
