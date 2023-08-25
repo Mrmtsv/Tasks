611d0b45d3e95ca89c0ab29fb33932c2c897b944
Vladimir <bajiau@mail.ru>
2023-08-25 22:05:32 +0300

    Correkted Task030

--- Task030/Program.cs
+++ Task030/Program.cs
@@ -1,6 +1,6 @@
-﻿// int array1 = new int[8] {5,7,8,5,23,87,12,1};
-// int array2 = new int[] {5,7,8,5,23,87,12,1};
-// int array3 = {5,7,8,5,23,87,12,1};
+﻿// int array1 = new int[8] {5,7,8,5,23,87,12,1};    //заполнение отдельных элементов
+// int array2 = new int[] {5,7,8,5,23,87,12,1};     //заполнение отдельных элементов
+// int array3 = {5,7,8,5,23,87,12,1};               //заполнение отдельных элементов
 // var array4 = new int[8];
 
 // int[] array = new int[8]; // 0 1 2 3 4 5 6 7
@@ -8,13 +8,13 @@
 // array[6] = 6456;    //заполнение отдельных элементов
 // array[2] = 34;      //заполнение отдельных элементов
 
-// //FillArray (int[] array)
+// //FillArray (int[] array)        //заполнение через функцию
 // for (int i = 0; i < array.Length; i++)
 // {
 //     array[i] = i * i;
 // }
 
-// //PrintArray
+// //PrintArray                     //печать через функцию
 // for (int i = 0; i < array.Length; i++)
 // {
 //     Console.Write(array[i] + " ");
@@ -28,9 +28,10 @@
 void FillArr(int[] mass)
 {
     int i = 0;
+    var random = new Random();
     while (i < mass.Length)
     {
-        mass[i] = new Random().Next(2);
+        mass[i] = random.Next(2);
         i++;
     }
 }
@@ -40,10 +41,11 @@ void PrintArr(int[] mass)
     int i = 0;
     while (i < mass.Length)
     {
-        Console.Write(mass[i] + " ");
+        Console.Write($"{mass[i]} ");
         i++;
     }
 }
+
 int[] array = new int[8];
 FillArr(array);
 PrintArr(array);
