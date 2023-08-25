8474113f1c87287cd6cfe1120d75ecb299aa40a0
Vladimir <bajiau@mail.ru>
2023-08-25 20:51:12 +0300

    Corrected Task024

--- Task024/Program.cs
+++ Task024/Program.cs
@@ -5,30 +5,6 @@
 // 4 -> 10
 // 8 -> 36
 
-// int SumNumbers(num)
-// {
-//     int sum = 0;
-//     for(int i = 1; i <= num; i++)
-//     {
-//         checked
-//         sum += i;
-//     }
-//     return sum;
-// }
-
-// Console.WriteLine("Введите натуральное число");
-// int number = Convert.ToInt32(Console.ReadLine());
-
-// int SumNumbers(number);
-
-// if(number < 1)
-// {
-//     Console.WriteLine("Введено не корректное число");
-//     return
-// }
-
-// Console.WriteLine($"Сумма чисел от 1 до {number} = {SumNumbers}");
-
 int SumNumbers(int num)
 {
     int sum = 0;
