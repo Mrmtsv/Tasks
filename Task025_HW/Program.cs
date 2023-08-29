9476548fd59d8cd543d3ac4c80246f869bb1635d
MuromtsevVV <bajiau@mail.ru>
2023-08-29 10:37:58 +0300

    Added Task025_HW

---
+++ Task025_HW/Program.cs
@@ -0,0 +1,21 @@
+﻿//  25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+int ExponentiationAB(int num1, int num2)
+{
+    int res = 1;
+    int i = 0;
+    while (i < num2)
+    {
+        res *= num1;
+        i++;
+    }
+    return res;
+}
+
+Console.WriteLine("Введите число A");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+int result = ExponentiationAB(numberA, numberB);
+Console.WriteLine($"Число {numberA} в степени {numberB} равно {result}.");
