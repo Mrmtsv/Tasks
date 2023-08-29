74c6339389e4230bf56fef3af864f0dda463a3bb
MuromtsevVV <bajiau@mail.ru>
2023-08-29 11:02:55 +0300

    Added Task027_HW

---
+++ Task027_HW/Program.cs
@@ -0,0 +1,18 @@
+﻿// 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+int SumDigNum(int num)
+{
+    int res = 0;
+    while (num != 0)
+    {
+        res += (num % 10);
+        num /= 10;
+    }
+    return res;
+}
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int result = SumDigNum(number);
+Console.WriteLine($"Сумма цифр числа {number} равна {result}.");
