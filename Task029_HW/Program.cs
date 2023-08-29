0ef703f1ef1ad5e804aa63f7231289ffa7b9bdd6
MuromtsevVV <bajiau@mail.ru>
2023-08-29 12:27:58 +0300

    Added Task029_HW

---
+++ Task029_HW/Program.cs
@@ -0,0 +1,32 @@
+﻿// 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+void FillArr(int[] mass)
+{
+    int i = 0;
+    while (i < mass.Length)
+    {
+        mass[i] = new Random().Next(-100, 100);
+        i++;
+    }
+}
+
+void PrintArr(int[] mass)
+{
+    int i = 0;
+    while (i < mass.Length)
+    {
+        Console.Write(mass[i] + " ");
+        i++;
+    }
+    Console.Write("-> [");
+    int j = 0;
+    while (j < (mass.Length - 1))
+    {
+        Console.Write(mass[j] + " ");
+        j++;
+    }
+    Console.Write($"{mass[mass.Length - 1]}]");
+}
+int[] array = new int[8];
+FillArr(array);
+PrintArr(array);
