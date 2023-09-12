917a64a4b4b5a96bb397b1c88ef92f88d6cecb2c
Vladimir <bajiau@mail.ru>
2023-09-12 17:36:35 +0300

    Added Task046, ..048, ..049, ..051

---
+++ Task049/Program.cs
@@ -0,0 +1,51 @@
+﻿// 49: Задайте двумерный массив. Найдите элементы, у
+// которых оба индекса чётные, и замените эти элементы на их
+// квадраты.
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    //                        0       1
+    int[,] matrix = new int[rows, columns]; // rows = 3, columns = 4
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++) // 3
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++) // 4
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintArray(int[,] arr)
+{
+    int rows = arr.GetLength(0);
+    int columns = arr.GetLength(1);
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            Console.Write($"{arr[i, j],5}");
+        }
+        Console.WriteLine();
+    }
+}
+
+void EvenIndexesInSquare(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i += 2)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j += 2)
+        {
+            matrix[i, j] *= matrix[i, j];
+        }
+    }
+}
+
+int[,] matrix = CreateMatrixRndInt(4, 4, 0, 10);
+PrintArray(matrix);
+EvenIndexesInSquare(matrix);
+Console.WriteLine();
+PrintArray(matrix);
