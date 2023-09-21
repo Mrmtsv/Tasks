c496244ee1362fd4a0f28d44c22974096580ab8f
Vladimir <bajiau@mail.ru>
2023-09-22 02:16:42 +0300

    Added Task064_HW & Task066_HW

---
+++ Task064_HW/Program.cs
@@ -0,0 +1,21 @@
+﻿/*64: Задайте значение N. Напишите программу, которая выведет все натуральные числа
+в промежутке от N до 1. Выполнить с помощью рекурсии.*/
+
+Console.WriteLine("Задайте значение N.");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+if (numberN < 1)
+{
+    System.Console.WriteLine("Некорректный ввод!");
+    return;
+}
+
+Console.Write($"N = {numberN} -> ");
+ReverseNaturalNumbers(numberN);
+
+void ReverseNaturalNumbers(int num)
+{
+    if (num == 0) return;
+    Console.Write($"{num} ");
+    ReverseNaturalNumbers(num - 1);
+}
---
+++ Task066_HW/Program.cs
@@ -0,0 +1,30 @@
+﻿/*66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных
+элементов в промежутке от M до N. Выполнить с помощью рекурсии.*/
+
+Console.WriteLine("Задайте значение M.");
+int numberM = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Задайте значение N.");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+if (numberM < 1 || numberN < 1)
+{
+    System.Console.WriteLine("Некорректный ввод!");
+    return;
+}
+
+int res = SumNaturalNumbersInRange(numberM, numberN);
+Console.Write($"M = {numberM}, N = {numberN} -> {res}");
+int SumNaturalNumbersInRange(int from, int to)
+{
+    if (from == to) return from;
+
+    else if (from < to)
+    {
+        from += SumNaturalNumbersInRange(from + 1, to);
+    }
+    else if (from > to)
+    {
+        from += SumNaturalNumbersInRange(from - 1, to);
+    }
+    return from;
+}
